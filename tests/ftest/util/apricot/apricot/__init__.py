c819a289f669344c8f3646bda73139826395bccb
Samir <samir.raval@intel.com>
2019-05-01 23:49:19 +0000

    delete all

--- tests/ftest/util/apricot/apricot/__init__.py
+++
@@ -1,6 +0,0 @@
-''' apricot __init__ '''
-__all__ = ['Test', 'TestWithServers', 'TestWithoutServers', 'TestWithClients',
-           'skipForTicket']
-
-from apricot.test import Test, TestWithServers, TestWithoutServers
-from apricot.test import TestWithClients, skipForTicket
